c4d850b1a59f2852078e129c2139d5e0c1839ee7
Mariam <kelanimaryam1234@gmail.com>
2024-11-30 06:53:02 +0100

    edited the text in navbar component in .

--- Desktop/react-tutorial/dashboard_1/src/Home.js
+++ Desktop/react-tutorial/dashboard_1/src/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="home">
-      <h2>Welcome to Maykay</h2>
+      <h2>Welcome to Maykay Homepage</h2>
     </div>
   );
 };
--- Desktop/react-tutorial/dashboard_1/src/Navbar.js
+++ Desktop/react-tutorial/dashboard_1/src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Welcome,Nav links here</h1>
+      <h1>Welcome,Nav links maykay blog</h1>
       <div className="links">
         Click <a href="/">Home</a> <br />
         <a href="/create">New Blog</a>
